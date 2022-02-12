d9122085fe5b303f1ae160c087200087d91af0bf
Andrey Brovchenko <andrebrjob@mail.ru>
2022-02-12 19:21:50 +0300

    HW 1.2.1 and HW 1.2.2

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,7 @@
+import csv
+
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -9,9 +13,17 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-
+    page_number = int(request.GET.get('page', 1))
+    bus_stations_list = []
+    with open(settings.BUS_STATION_CSV, encoding='utf-8', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            bus_stations_tuple = {'Name': row['Name'], 'Street': row['Street'], 'District': row['District']}
+            bus_stations_list.append(bus_stations_tuple)
+    paginator = Paginator(bus_stations_list, 10)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,12 +19,29 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def omlet_view(request):
+    servings = int(request.GET.get("servings", 1))
+    recipe = DATA['omlet'].copy()
+    for key, value in recipe.items():
+        recipe[key] = value * servings
+    context = {'recipe': recipe}
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta_view(request):
+    servings = int(request.GET.get("servings", 1))
+    recipe = DATA['pasta'].copy()
+    for key, value in recipe.items():
+        recipe[key] = value * servings
+    context = {'recipe': recipe}
+    return render(request, 'calculator/index.html', context)
+
+
+def buter_view(request):
+    servings = int(request.GET.get("servings", 1))
+    recipe = DATA['buter'].copy()
+    for key, value in recipe.items():
+        recipe[key] = value * servings
+    context = {'recipe': recipe}
+    return render(request, 'calculator/index.html', context)
